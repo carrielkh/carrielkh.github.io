feb03c49d56ed0f97532891a94c6124c9f539222
carrielkh <kehuiliu@Kehuis-MBP.home>
2014-11-02 10:26:30 -0500

    new

---
+++ js/map.js
@@ -0,0 +1,55 @@
+var geocoder;
+    var map;
+
+
+    function initialize() {
+
+      geocoder = new google.maps.Geocoder();
+      var myLatLng = new google.maps.LatLng(-33.890542, 151.274856);
+    
+      var styles = [
+       {
+         stylers: [
+         { visibility: 'simplified' },
+         { gamma: 2 },
+         { weight: 2 }
+         ]
+       },
+        {
+       featureType: "water",
+       stylers: [
+        { color: "#69C3CC" },
+       ]
+        },
+        {
+       featureType: "road",
+       stylers: [
+        { visibility: "off" }
+       ]
+        },
+        {
+         featureType: "landscape",
+         stylers: [
+          { color:"#AEBC6A"}
+         ]
+        }
+      ];
+    
+      var styledMap = new google.maps.StyledMapType(styles,
+       {name: "Styled Map"});
+
+      var mapOptions = {
+      zoom: 2,
+      center: new google.maps.LatLng(40.711877, -73.992423),
+      mapTypeControl: false,
+      streetViewControl:false,
+      zoomControl: true,
+      };
+
+      map = new google.maps.Map(document.getElementById('googleMap'),mapOptions);
+
+      map.mapTypes.set('map_style', styledMap);
+      map.setMapTypeId('map_style');
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
