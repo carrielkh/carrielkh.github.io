d19306401ade08e15c9c6693b718d95e1b9b731b
carrielkh <kehuiliu@Kehuis-MacBook-Pro.local>
2014-11-13 10:26:03 -0500

    ／

--- test1/upload1.php
+++ test1/upload1.php
@@ -1,30 +1,83 @@
 <?php
-$servername = 'localhost';
-$username = 'root';
-$password = 'root';
-$dbname = 'forms1';
 
-// Create connection
-$conn = mysql_connect($servername, $username, $password);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-mysql_select_db("forms1",$conn);
+if($_POST["password"]==$_POST["passwordagain"] )
+{
 
+$con=mysql_connect("localhost","root","root");
 
+if(!$con)
+
+  {
+
+  die('Error connecting to server :'.mysql_error());
+
+  }
+mysql_select_db("forms1",$con);
 $value1 = $_POST['email'];
 $value2 = $_POST['password'];
 $value3 = $_POST['passwordagain'];
+$value4 = $_POST['uname'];
 
-$sql = "INSERT INTO upload1 (email, password, passwordagain)
-VALUES ('$value1', '$value2', '$value3')";
+$result=mysql_query("SELECT email FROM upload1 WHERE email='$value1'");
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+if(!mysql_fetch_array($result,MYSQL_ASSOC))
+
+  {
+
+$sql="INSERT INTO upload1(email, password,passwordagain,uname) VALUES('$value1','$value2','$value3','$value4')";
+
+if(!mysql_query($sql,$con))
+
+  {
+
+  die('Error :'.mysql_error());
+
+  }
+
+ echo "You are registered. Congrats !!!";
+
+
+
+ }
+
+else
+
+ {
+
+ echo "Username Already exists ! ";
+
+
+
+ }
+
+ mysql_close($con);
+
 }
 
-$conn->close();
+else
+
+{
+
+if($_POST["password"]!=$_POST["passwordagain"]){
+
+  echo "Password mismatch <br />";}
+
+else
+
+{  echo "Invalid Username  <br />";
+
+echo "<a href='homepage.html'> Go back to login page </a>";}
+
+}
+
+/*
+39
+1) We shouldnt allow similar usernames. (Y)
+40
+2) make the go back to login page work. (Y)
+41
+3) We shouldnt allow null usernames and passwords.(Y)
+42
+*/
+
 ?>
---
+++ test1/upload2.php
@@ -0,0 +1,43 @@
+<?php
+
+    session_start();
+
+$con=mysql_connect("localhost","root","root");
+
+if(!$con)
+
+  {
+
+  die('Error connecting to server :'.mysql_error());
+
+  }
+
+mysql_select_db("forms1",$con);
+
+$result=mysql_query("SELECT email,password FROM upload1 WHERE email='$_POST[email]' && password='$_POST[password]'");
+
+if(!mysql_fetch_array($result,MYSQL_ASSOC))
+
+  {
+
+  echo "Wrong Login. Go back and try again.";
+
+  
+
+  }
+
+else
+
+  {
+
+
+ header('Location: map.html');
+
+   
+
+   
+
+  }
+
+ mysql_close($con);
+ ?>
