cd7ad3bbef671d92b6ac334bc89aceaf9a2dce52
carrielkh <carrielkh@users.noreply.github.com>
2014-12-11 15:18:19 -0500

    Update map1.js

--- destination/js/map1.js
+++ destination/js/map1.js
@@ -1,13 +1,9 @@
 var geocoder;
-    var map;
-
-
-    function initialize() {
+var map;
+function initialize() {
       signed_in=true
-
       geocoder = new google.maps.Geocoder();
       var myLatLng = new google.maps.LatLng(-33.890542, 151.274856);
-    
       var styles = [
        {
          stylers: [
@@ -35,22 +31,16 @@ var geocoder;
          ]
         }
       ];
-    
-      var styledMap = new google.maps.StyledMapType(styles,
-       {name: "Styled Map"});
-
+      var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
       var mapOptions = {
-      zoom: 2,
-      center: new google.maps.LatLng(40.711877, -73.992423),
-      mapTypeControl: false,
-      streetViewControl:false,
-      zoomControl: true,
-      };
-
+         zoom: 2,
+         center: new google.maps.LatLng(40.711877, -73.992423),
+         mapTypeControl: false,
+         streetViewControl:false,
+         zoomControl: true,
+        };
       map = new google.maps.Map(document.getElementById('googleMap1'),mapOptions);
-
       map.mapTypes.set('map_style', styledMap);
       map.setMapTypeId('map_style');
     }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
