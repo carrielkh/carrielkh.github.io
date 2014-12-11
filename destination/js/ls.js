79052e5ea5070d70d4f4b47a4383711c9eaac484
carrielkh <carrielkh@users.noreply.github.com>
2014-12-11 15:14:13 -0500

    Update ls.js

--- destination/js/ls.js
+++ destination/js/ls.js
@@ -1,11 +1,14 @@
 function login() {
     window.open("map.html", "_self");
     }
-    function loginpage() {
+function loginpage() {
     window.open("home1.php", "_self");
     }
 function signup() {
     window.open("signup.php", "_self");
+    }
+function explore() {
+    window.open("explore.html");
     }
 function select() {
     document.getElementById("text").select();
