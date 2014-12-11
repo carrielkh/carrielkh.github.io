a9fc8c619224682f23df4fa564c459587ca72e86
carrielkh <carrielkh@users.noreply.github.com>
2014-12-11 15:17:04 -0500

    Update map.js

--- destination/js/map.js
+++ destination/js/map.js
@@ -1,12 +1,8 @@
 var geocoder;
-    var map;
-
-
-    function initialize() {
-
+var map;
+function initialize() {
       geocoder = new google.maps.Geocoder();
       var myLatLng = new google.maps.LatLng(-33.890542, 151.274856);
-    
       var styles = [
        {
          stylers: [
@@ -15,41 +11,29 @@ var geocoder;
          { weight: 2 }
          ]
        },
-        {
-       featureType: "water",
-       stylers: [
-        { color: "#69C3CC" },
-       ]
-        },
-        {
-       featureType: "road",
-       stylers: [
-        { visibility: "off" }
-       ]
-        },
-        {
+       {
+         featureType: "water",
+         stylers: [{ color: "#69C3CC" },]
+       },
+       {
+         featureType: "road",
+         stylers: [{ visibility: "off" }]
+       },
+       {
          featureType: "landscape",
-         stylers: [
-          { color:"#AEBC6A"}
-         ]
-        }
+         stylers: [{ color:"#AEBC6A"}]
+       }
       ];
-    
-      var styledMap = new google.maps.StyledMapType(styles,
-       {name: "Styled Map"});
-
+      var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
       var mapOptions = {
-      zoom: 2,
-      center: new google.maps.LatLng(40.711877, -73.992423),
-      mapTypeControl: false,
-      streetViewControl:false,
-      zoomControl: true,
-      };
-
+         zoom: 2,
+         center: new google.maps.LatLng(40.711877, -73.992423),
+         mapTypeControl: false,
+         streetViewControl:false,
+         zoomControl: true,
+        };
       map = new google.maps.Map(document.getElementById('googleMap'),mapOptions);
-
       map.mapTypes.set('map_style', styledMap);
       map.setMapTypeId('map_style');
     }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
